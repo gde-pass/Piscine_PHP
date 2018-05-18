96167089e3a817276ec6832ebbbfe2ab486ed159
Gregory DE PASS <gde-pass@e2r13p3.42.fr>
2018-05-18 18:57:34 +0200

    ex06 fini

---
+++ day01/ex06/ssap.php
@@ -0,0 +1,17 @@
+#!/usr/bin/php
+<?php
+$i = 1;
+
+while($i < $argc)
+{
+    $array .= $argv[$i];
+    $array .= " ";
+    $i++;
+}
+
+$array = preg_split("/ +/", trim($array));
+sort($array);
+
+foreach ($array as $value)
+echo "$value", PHP_EOL;
+?>
