f9761b57e336ee7f43d06a771316c6483c7d9279
Gregory DE PASS <gde-pass@e2r13p3.42.fr>
2018-05-18 17:45:27 +0200

    ft_split fini

--- day01/ex03/ft_split.php
+++ day01/ex03/ft_split.php
@@ -1,6 +1,11 @@
 #!/usr/bin/php
 <?php
+function ft_split($chain)
+{
+    $chain = preg_split("/ /", $chain, NULL, PREG_SPLIT_NO_EMPTY);
+    sort($chain);
+    return ($chain);
+}
 
-
-
+print_r(ft_split("Hello      World AAA"));
 ?>
