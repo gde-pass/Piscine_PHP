9eacbbfd4d1c2738a237a7f8767d8fc1851d40e7
GREGORY <gde-pass@student.42.fr>
2018-05-20 18:44:26 +0200

    ex 10 fini

---
+++ day01/ex10/do_op.php
@@ -0,0 +1,42 @@
+#!/usr/bin/php
+<?php
+
+// Check si le nombre d'arguments est respecté puis
+// Récupere tous les arguments et les places dans un tableaux en suppriment les
+// espaces en trop au milleu et sur les cotés.
+
+if ($argc != 4)
+    echo "Incorrect Parameters", PHP_EOL;
+else
+{
+    $i = 1;
+    while($i < $argc)
+    {
+        $string .= $argv[$i];
+        $string .= " ";
+        $i++;
+    }
+    $array = preg_split("/ +/", trim($string));
+
+// Fait l'opération en fonction de argv[1]
+
+    switch ($array[1])
+    {
+        case '+':
+            echo $array[0] + $array[2];
+            break;
+        case '-':
+            echo $array[0] - $array[2];
+            break;
+        case '*':
+            echo $array[0] * $array[2];
+            break;
+        case '/':
+            echo $array[0] / $array[2];
+            break;
+        case '%':
+            echo $array[0] % $array[2];
+            break;
+    }
+}
+?>
