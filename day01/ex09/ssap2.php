a673d83c4f959767c26017d94ec82503eadfa295
GREGORY <gde-pass@student.42.fr>
2018-05-20 17:50:33 +0200

    ex 09 fini

--- day01/ex09/ssap2.php
+++ day01/ex09/ssap2.php
@@ -1,4 +1,36 @@
 #!/usr/bin/php
 <?php
+$i = 1;
+while($i < $argc)
+{
+	$string .= $argv[$i];
+	$string .= " ";
+	$i++;
+}
+$array = preg_split("/ +/", trim($string));
 
+foreach ($array as $value)
+{
+	if (ctype_digit($value))
+	{
+		$digit_array[] = $value;
+		unset($array[array_search($value, $array)]);
+	}
+	if (ctype_alpha($value))
+	{
+		$alpha_array[] = $value;
+		unset($array[array_search($value, $array)]);
+	}
+}
+
+natcasesort($alpha_array);
+sort($digit_array, SORT_STRING);
+sort($array, SORT_STRING);
+
+foreach ($alpha_array as $value)
+	echo "$value", PHP_EOL;
+foreach ($digit_array as $value)
+	echo "$value", PHP_EOL;
+foreach ($array as $value)
+	echo "$value", PHP_EOL;
 ?>
