aec8b0f7ac851ba9a9b673c4f2da00246e5318c1
GREGORY <gde-pass@student.42.fr>
2018-05-29 00:01:25 +0200

    ex01 fix

--- day04/ex01/create.php
+++ day04/ex01/create.php
@@ -1,34 +1,45 @@
 <?php
-function check_content($file)
+function already_registered()
 {
-    foreach ($file as $login)
+    $array = file_get_contents('../htdocs/private/passwd');
+    $unserialized_array = unserialize($array);
+    foreach ($unserialized_array as $element)
     {
-        if ($login == $_POST['login'])
-            return true;
+        if ($element['login'] == $_POST['login'])
+            return TRUE;
         else
-            return false;
+            return FALSE;
     }
+}
+
+function add_user($hashed_pw)
+{
+    $array = file_get_contents('../htdocs/private/passwd');
+    $unserialized_array = unserialize($array);
+    $unserialized_array[] = array('login' => $_POST['login'], 'passwd' => $hashed_pw);
+    $serialized_array = serialize($unserialized_array);
+    file_put_contents('../htdocs/private/passwd', $serialized_array);
+    echo "OK", PHP_EOL;
 }
 
 if ($_POST['submit'] === 'OK' && $_POST['login'] != NULL && $_POST['passwd'] != NULL)
 {
+    $hashed_pw = hash('whirlpool', '$_POST[\'passwd\']');
     if (!file_exists('../htdocs/private'))
     {
         mkdir('../htdocs/private/', 0777, true);
-        file_put_contents('../htdocs/private/passwd', '', FILE_APPEND);
-    }
-
-    $tab['login'] = $_POST['login'];
-    $tab['passwd'] = hash('whirlpool', '$_POST[\'passwd\']');
-    $file = unserialize(file_get_contents('../htdocs/private/passwd'));
-
-    if (check_content($file) == false)
-    {
-        file_put_contents('../htdocs/private/passwd', serialize($tab), FILE_APPEND);
-        echo "OK\n";
+        $array = array(array('login' => $_POST['login'], 'passwd' => $hashed_pw));
+        $serialized_array = serialize($array);
+        file_put_contents('../htdocs/private/passwd', $serialized_array);
+        echo "OK", PHP_EOL;
     }
     else
-        echo "ERROR\n";
+    {
+        if (already_registered() == TRUE)
+            echo "ERROR\n";
+        else
+            add_user($hashed_pw);
+    }
 }
 else
     echo "ERROR\n";
