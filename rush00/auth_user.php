8791b8f3d1bc392447d2117e8040a3b50410a556
Gregory DE PASS <gde-pass@e1r10p21.42.fr>
2018-05-31 21:46:56 +0200

    formulaire de connexion et possibilité de se connecter

---
+++ rush00/auth_user.php
@@ -0,0 +1,25 @@
+<?php
+session_start();
+include 'install.php';
+if (!(file_exists('private/users'))) // Creation du fichier users et create admin account
+    create_admin_account();
+
+function auth($login, $passwd)
+{
+    $passwd = hash('whirlpool', $passwd);
+    $array = file_get_contents('private/users');
+    $array = unserialize($array);
+    foreach ($array as $account)
+    {
+        if ($login === $account['login'] AND $passwd === $account['passwd'])
+            return TRUE;
+    }
+    return FALSE;
+}
+
+if (auth($_POST['login'], $_POST['passwd']) == TRUE)
+{
+    $_SESSION['connexion_status'] = 'connected';
+    $_SESSION['login'] = $_POST['login'];
+}
+?>
