ed5af37adb5bd4d47f8922ae9121c1c4cddf5eb6
Gregory DE PASS <gde-pass@e2r4p2.42.fr>
2018-06-02 15:21:06 +0200

    fix disconnexion

--- rush00/disconnexion.php
+++ rush00/disconnexion.php
@@ -1,14 +1,6 @@
 <?php
 session_start();
-if ($_SESSION['login'] == '' OR $_SESSION['connexion_status'] != 'connected')
-{
-    header('HTTP/1.0 401 Unauthorized');
-    header('Location: index.php');
-}
-else
-{
-    $_SESSION = array();
-    $_SESSION['connexion_status'] = 'disconnected';
-    header('Location: index.php');
-}
+$_SESSION = array();
+$_SESSION['connexion_status'] = 'disconnected';
+header('Location: index.php');
 ?>
