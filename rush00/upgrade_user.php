fbcf16c9ff2c2a41010fb0ddc4d78dea4d85d0e5
Gregory DE PASS <gde-pass@e2r12p1.42.fr>
2018-06-02 05:25:41 +0200

    Upgrade user fonctionnel

--- rush00/upgrade_user.php
+++ rush00/upgrade_user.php
@@ -1,5 +1,38 @@
 <?php
 session_start();
+
+function check_admin()
+{
+    $pass = hash('whirlpool', $_POST['password']);
+    $array = file_get_contents('private/users');
+    $unserialized_array = unserialize($array);
+    foreach ($unserialized_array as $element)
+    {
+        if ($element['login'] === $_POST['admin_login'] AND $element['admin'] === 'yes' AND $pass == $element['passwd'])
+            return TRUE;
+    }
+    return FALSE;
+}
+
+function upgrade_user($user)
+{
+    $i = 0;
+    $array = file_get_contents('private/users');
+    $unserialized_array = unserialize($array);
+    foreach ($unserialized_array as $account)
+    {
+        if ($_POST['user_login'] === $account['login'])
+        {
+            $unserialized_array[$i]['admin'] = 'yes';
+            $serialized_array = serialize($unserialized_array);
+            file_put_contents('private/users', $serialized_array);
+            return TRUE;
+        }
+        $i++;
+    }
+    return FALSE;
+}
+
 if ($_SESSION['login'] == '' OR $_SESSION['connexion_status'] != 'connected' OR $_SESSION['admin'] == 'no')
 {
     header('HTTP/1.0 401 Unauthorized');
@@ -7,9 +40,15 @@ if ($_SESSION['login'] == '' OR $_SESSION['connexion_status'] != 'connected' OR 
 }
 else
 {
-    
+    if ($_POST['submit'] === 'OK' AND $_POST['user_login'] != NULL AND $_POST['admin_login'] != NULL AND $_POST['password'] != NULL)
+    {
+        if (check_admin() == TRUE)
+        {
+            upgrade_user($_POST['user_login']);
+            header('Location: admin_only.php');
+        }
+        else
+            echo "ERROR\n";
+    }
 }
-
-
-
 ?>
