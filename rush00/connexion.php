cd4472fe08aef7fd0c26bac0546d5a6234f5195f
LE GALL Maxence <legallmaxence@MBP-Max.local>
2018-06-02 02:36:45 +0200

    ajout des messages d'erreur directement sur les pages de login etc pour remplacer les pages d'erreur

--- rush00/connexion.php
+++ rush00/connexion.php
@@ -4,6 +4,13 @@ include "header.php";
     <div class=contentform>
     <form class="formbg" action="auth_user.php" method="post">
     <p>
+        <?php
+        if($_GET["action"] === "create")
+            echo("<div class=msgsucces><a>Your account has been created, you may log-in !</a></div>");
+        else if(à)
+            if($_GET["action"] === "error")
+            echo("<div class=msgerror><a>ERROR : Invalid Login/Password</a></div>");
+        ?>
         <h1>Log-In</h1>
         <input class=forminput placeholder="Login" type="text" name="login" autofocus required />
         <br>
--- rush00/inscription.php
+++ rush00/inscription.php
@@ -5,7 +5,14 @@ include "header.php";
 			<form class="formbg" action="create_user.php" method="post">
 			<h1>Sign-In</h1>
 			<p>
-				<input class=forminput placeholder="Login" type="text" name="login" autofocus required />
+				<?php
+        		if($_GET["action"] === "error")
+            	echo('<div class=msgerror><a>ERROR : The two password don\'t match</a></div><input class=forminput placeholder="Login" type="text" name="login" value='.$_GET['login']." autofocus required />");
+            	else if ($_GET["action"] === "error2")
+            	echo('<div class=msgerror><a>ERROR : Login already in use</a></div><input class=forminput placeholder="Login" type="text" name="login" autofocus required />');
+            	else
+            		echo('<input class=forminput placeholder="Login" type="text" name="login" autofocus required />');
+        		?>
 				<br>
 				<input class=forminput placeholder="Password" type="password" name="passwd1" required />
 				<br>
