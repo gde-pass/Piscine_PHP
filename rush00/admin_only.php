e15120ba857378cc1dacd486db71c85dade14658
Gregory DE PASS <gde-pass@e2r12p1.42.fr>
2018-06-02 05:49:07 +0200

    ajout de quelques articles et creation de deux nouveaux form

--- rush00/admin_only.php
+++ rush00/admin_only.php
@@ -22,6 +22,39 @@ if ($_SESSION['login'] == '' OR $_SESSION['connexion_status'] != 'connected' OR 
     </p>
     </form>
 </div>
+
+<div class=contentform>
+    <form class="formbg" action="modify_article.php" method="post">
+    <p>
+        <h1>Modify article(a faire)</h1>
+        <strong>if you let a empty line the old value will stay</strong>
+        <br>
+        <input class=forminput placeholder="Old Title" type="text" name="old_title" autofocus required />
+        <br>
+        <input class=forminput placeholder="New Title" type="text" name="new_title" />
+        <br>
+        <input class=forminput placeholder="URL Image" type="url" name="img"  />
+        <br>
+        <input class=forminput placeholder="Price" type="number" name="price" step="0.01"  />
+        <br>
+        <input class=forminput placeholder="Description" type="text" name="description" />
+        <br>
+        <input class=buttoninput type="submit" name="submit" value="OK" />
+    </p>
+    </form>
+</div>
+
+<div class=contentform>
+    <form class="formbg" action="delete_article.php" method="post">
+    <p>
+        <h1>Delete article (a faire)</h1>
+        <input class=forminput placeholder="Title" type="text" name="title" autofocus required />
+        <br>
+        <input class=buttoninput type="submit" name="submit" value="OK" />
+    </p>
+    </form>
+</div>
+
 <div class=contentform>
     <form class="formbg" action="upgrade_user.php" method="post">
     <p>
