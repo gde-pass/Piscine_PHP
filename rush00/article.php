10fdb9d725cdd9302354c7167156c99a92630fbc
Maxence LE GALL <male-gal@e2r5p3.42.fr>
2018-06-02 21:02:15 +0200

    modification de la page article

--- rush00/article.php
+++ rush00/article.php
@@ -7,6 +7,9 @@ if ($_GET["article"] && isset($_GET["article"]))
 		if ($value['title'] === $_GET["article"])
 		{
 			$artname = $_GET["article"];
+			$description = $value["description"];
+			$price = $value["price"];
+			$img = $value["img"];
 		}
 	}
 	if(!isset($artname))
@@ -21,12 +24,24 @@ else
     header('Location: index.php');
 }
 ?>
-		<div class=catcontent>
-			<div class=latestarticles>
-				<h2><?php echo($artname);?></h2>
+		<div class=artcontent>
+			<div class=articlebg>
+				<div class=topbox>
+					<div class=artleftcol>
+						<h2><?php echo($artname);?></h2>
+						<div class=descr><a><?php echo($description);?></a></div>
+					</div>
+					<div class=artrightcol>
+						<div class=articleimg><img src=<?php echo $img;?>></div>
+						<div class=price><h1><?php echo($price);?></h1></div>
+					</div>
+				</div>
+				<div class=botbox>
+					<div class=tocart>
+						<input class=buttoncart type="submit" name="submit" value="ADD TO CART" />
+					</div>
+				</div>
 			</div>
-		<div class=articles>
-			
 		</div>
 <?php
 include 'footer.php';
