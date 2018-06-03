a68505b0c52bfff871d842251c0b6282a2889208
Maxence LE GALL <male-gal@e2r5p3.42.fr>
2018-06-03 03:44:25 +0200

    Page article completement cree

--- rush00/cart.php
+++ rush00/cart.php
@@ -4,18 +4,36 @@ include 'header.php';
 
 <div class=cartcontent>
 	<div class=cart>
-		<h1>Shopping Cart</h1>
-		<div class=line>
-			<div class=name>
-				<a>$nomproduit</a>
+		<div class=title>
+			<h1>Shopping Cart</h1>
+		</div>
+		<div class=lines>
+			<div class=line>
+				<div class=name>
+					<a>$nomproduit</a>
+				</div>
+				<div  class=space1></div>
+				<div class=quantity>
+					<a> Quantity : $quantity</a>
+				</div>
+				<div class=buttons>
+					<input class=cartbutton type="submit" name="submit" value="-"/>
+					<input class=cartbutton type="submit" name="submit" value="+"/>
+				</div>
+				<div  class=sapce></div>
+				<div class=price>
+					<a> Price : $price</a>
+				</div>
 			</div>
-			<div class=quantity>
-			<a> Quantity : $quantity</a>
+		</div>
+		<div class=bottom>
+			<div class=totalprice>
+				<a>Total price :  </a> <h2>$_price €</h2>
 			</div>
-			<div class=price>
-				<a> Price : $price</a>
+			<div class=orderbutton>
+				<input class=buttoninput type="submit" name="submit" value="Acheter"/>
 			</div>
-			<input class=cartbutton type="submit" name="submit" value="add 1" />
+			</div>
 		</div>
 	</div>
 </div>
