b0bed074c3eb1618b383043a4f74af2727e99a83
Maxence LE GALL <male-gal@e2r5p3.42.fr>
2018-06-03 02:18:47 +0200

    style and cart changes

--- rush00/cart.php
+++ rush00/cart.php
@@ -3,16 +3,22 @@ include 'header.php';
 ?>
 
 <div class=cartcontent>
-			<div class=cart>
-				<h1>Shopping Cart</h1>
+	<div class=cart>
+		<h1>Shopping Cart</h1>
+		<div class=line>
+			<div class=name>
+				<a>$nomproduit</a>
 			</div>
-			<div class=line>
-				<div class=name>
-					<a></a>
-				</div>
+			<div class=quantity>
+			<a> Quantity : $quantity</a>
 			</div>
+			<div class=price>
+				<a> Price : $price</a>
+			</div>
+			<input class=cartbutton type="submit" name="submit" value="add 1" />
+		</div>
+	</div>
 </div>
-
 <?php
 include 'footer.php';
 ?>
