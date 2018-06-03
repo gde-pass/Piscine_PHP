3051a9699b43da426c24178403151c95fdd169e6
Maxence LE GALL <male-gal@e2r5p3.42.fr>
2018-06-03 21:02:04 +0200

    Final changes ??!!!!

--- rush00/buy.php
+++ rush00/buy.php
@@ -6,25 +6,19 @@ if (isset($_SESSION['list']))
 {
     $list = $_SESSION['list'];
 }
-print_r($articles);
 foreach($list as $key => $elem)
 {
     $i = 0;
     foreach($articles as $keye => $eleme)
     {
         if($eleme['title'] === $key)
-        {
-            echo($elem."<br />");
-            echo($articles[$i]['quantity']."<br />");
             $articles[$i]['quantity'] = ($articles[$i]['quantity'] - $elem);
-        }
         $i++;
     }
 }
-print_r($articles);
 unset($_SESSION['cart']);
 unset($_SESSION['artname']);
 $articles = serialize($articles);
 file_put_contents('private/articles', $articles);
-header("Location: index.php");
 ?>
+<meta http-equiv="refresh" content="0; URL=index.php">
