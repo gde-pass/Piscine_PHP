76904e56924b49b2a38546e5f9fa3d660b529327
Gregory DE PASS <gde-pass@e2r11p6.42.fr>
2018-06-03 19:47:58 +0200

    fix del categoies

--- rush00/del_Category.php
+++ rush00/del_Category.php
@@ -20,9 +20,9 @@ if ($_SESSION['login'] == '' OR $_SESSION['connexion_status'] != 'connected' OR 
             <h1>Delete category</h1>
             <input class=forminput placeholder="Title" type="text" name="category" autofocus required />
             <br>
-            <input class=forminput placeholder="Your Login" type="text" name="category" autofocus required />
+            <input class=forminput placeholder="Your Login" type="text" name="admin_login" autofocus required />
             <br>
-            <input class=forminput placeholder="Your Password" type="text" name="category" autofocus required />
+            <input class=forminput placeholder="Your Password" type="text" name="password" autofocus required />
             <br>
             <input class=buttoninput type="submit" name="submit" value="OK" />
         </p>
